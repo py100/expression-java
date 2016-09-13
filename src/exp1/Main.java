f2c8c806afc256a7b2d2e25205a1dd8b0c308505
MTmotong <dzymt6627@163.com>
2016-09-13 11:44:25 +0800

    add simplify and derivative

--- src/exp1/Main.java
+++ src/exp1/Main.java
@@ -1,10 +1,7 @@
 package exp1;
-
 import java.util.*; 
 import java.util.Scanner;
 import java.util.Vector;
-
-
 
 // Node : 保存每一个项
 // Node.d 系数
@@ -12,10 +9,15 @@ import java.util.Vector;
 class Node {
 	String str;
 	int d;
-
 	Node(String s) {
 		this.str = "";
 		d = 1;
+		if (s.charAt(0) == '-')
+		{
+			d = -1;
+			s = s.substring(1);
+		}
+			
 		String[] tmp = s.split("\\*");
 		//tmp[].charAt()是未知数前面的系数　
 		for (int i = 0; i < tmp.length; i++) {
@@ -44,18 +46,87 @@ class Node {
 		}
 		str = tmp;
 	}
-
+	
+	Node simplify(char ch, int dig)
+	{
+		String tmp = "";
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str.charAt(i) == ch) d = d*dig;
+			else tmp = tmp+str.charAt(i);
+		}
+		str = tmp;
+		return this;
+	}
+	Node deri(char ch)
+	{
+		String tmp = "";
+		int n=0;
+		for(int i = 0;i<str.length();i++)
+		{
+			if(str.charAt(i) != ch)
+			{
+				tmp = tmp + str.charAt(i);
+			}
+			else
+			{
+				if(n > 0) 
+					tmp = tmp+str.charAt(i);
+				n++;
+			}
+		}
+		d = n*d;
+		str = tmp;
+		return this;
+	}
+	
 	void showNode() {
 		System.out.print(d);
 		for (int i = 0; i < str.length(); i++)
 			System.out.print("*" + str.charAt(i));
 	}
+	void showNode1()
+	{
+		//String tmp = "";
+		System.out.print(d);
+		char pchar = '\0';
+		int cnt = 0;
+		for(int i=0;i<str.length();i++)
+		{
+			if (str.charAt(i) != pchar)
+			{
+				if (cnt == 1) System.out.print("*"+pchar);
+				else if (cnt > 1)
+				{
+					System.out.print("*"+pchar+"^"+cnt);
+				}
+				pchar = str.charAt(i);
+				cnt = 1;
+			}
+			else
+				cnt++;
+		}
+		if (cnt == 1) System.out.print("*"+pchar);
+		else if(cnt > 1)
+		{
+			System.out.print("*"+pchar+"^"+cnt);
+		}
+	}
+
 }
 
 class expression {
 	Vector<Node> vn;
 
 	void init(String str) {
+		String tmp1 = "";
+		for(int i=0;i<tmp1.length();i++)
+		{
+			if(str.charAt(i) == '-')
+				tmp1=tmp1 + "+" + str.charAt(i);
+			else
+				tmp1 = tmp1 + str.charAt(i);
+		}
 		vn = new Vector<Node>();
 		String[] tmp = str.split("\\+");
 		for (int i = 0; i < tmp.length; i++) {
@@ -67,7 +138,7 @@ class expression {
 		for (int i = 0; i < vn.size(); i++) {
 			if (i != 0)
 				System.out.print("+");
-			vn.elementAt(i).showNode();
+			vn.elementAt(i).showNode1();
 		}
 		System.out.println();
 	}
@@ -102,13 +173,23 @@ class expression {
 		vn = tmp;
 		
 	}
-	void simplify()
+	void simplify(char ch, int dig)
+	{	
+		Vector<Node> tmp = new Vector<Node>(0);
+		for (int i = 0; i < vn.size(); i++)
+		{
+			tmp.addElement(vn.elementAt(i).simplify(ch, dig));
+		}
+		vn = tmp;
+	}
+	void deri(char ch)
 	{
-		Scanner sc = new Scanner(System.in);
-		//String sc = "!Simplify x=1,y=3";
-		sc = sc.substring(10);      
-		
-		
+		Vector<Node> tmp = new Vector<Node>(0);
+		for(int i=0;i<vn.size();i++)
+		{
+			tmp.addElement(vn.elementAt(i).deri(ch));
+		}
+		vn = tmp;
 	}
 	
 }
@@ -122,11 +203,33 @@ public class Main {
 		expression ex = new expression();
 		while (cin.hasNext()) {
 			cmd = cin.nextLine();
-
+			cmd = cmd.toLowerCase();
 			// get command
 			if (cmd.charAt(0) == '!') {
 				//!simplify simplify simplifyvar 1=num =num1... var n=num =numn
-				System.out.println("233");
+				//System.out.println("233");
+				// simplify
+				if (cmd.charAt(1) == 's')
+				{
+					String[] tmp = cmd.split("\\s");
+					tmp = tmp[1].split(",");
+					for (int i = 0; i < tmp.length; i++)
+					{
+						String[] tosimp = tmp[i].split("=");
+						char tmpchar = tosimp[0].charAt(0);
+						int dig = Integer.parseInt(tosimp[1]);
+						ex.simplify(tmpchar, dig);
+						ex.merge();
+					}
+				}
+				// d/dy
+				else
+				{
+					char cmdchar = cmd.charAt(4);
+					ex.deri(cmdchar);
+					ex.merge();
+				}	
+				ex.show();
 			}
 			// get expression
 			else {
