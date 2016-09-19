5afde3277642bbac2489be298fa106ffe21cc255
Epp11e <py100@live.cn>
2016-09-19 14:33:59 +0800

    add  input check for command

--- src/exp1/Main.java
+++ src/exp1/Main.java
@@ -255,38 +255,60 @@ public class Main {
 		while (cin.hasNext()) {
 			cmd = cin.nextLine();
 			cmd = cmd.toLowerCase();
+			//System.out.println(cmd);
 			// get command
 			if (cmd.charAt(0) == '!') {
 				// simplify
 				if (cmd.charAt(1) == 's') {
 					String[] tmp = cmd.split("\\s");
+					
+					if (!tmp[0].equals("!simplify")) {
+						System.out.println("Input Error!!\nNo such operation!!");
+						System.exit(0);
+//						continue;
+					}
 					tmp = tmp[1].split(",");
 					for (int i = 0; i < tmp.length; i++) {
+						tmp[i] = tmp[i].replace(" ", "");
 						String[] tosimp = tmp[i].split("=");
+						if (tosimp.length != 2 || tosimp[0].length() != 1 || (!(tosimp[0].charAt(0) >= 'a' && tosimp[0].charAt(0)<='z'))) {
+							System.out.println("Input Error!!\nNo such operation!!");
+							System.exit(0);
+						}
 						char tmpchar = tosimp[0].charAt(0);
-						int dig = Integer.parseInt(tosimp[1]);
+						int dig = 1;
+						try
+						{
+							dig = Integer.parseInt(tosimp[1]);
+						}catch(Exception e){
+							System.out.println("Input Error");
+							System.exit(0);
+						}
 						ex.simplify(tmpchar, dig);
 						ex.merge();
 					}
 				}
 				// d/dy
-				else {
+				else if (cmd.charAt(1) == 'd') {
+					if (cmd.length() != 5 || !cmd.substring(1, 4).equals("d/d")) {
+						System.out.println("Input Error!!\nNo such operation!!");
+						System.exit(0);
+					}
 					char cmdchar = cmd.charAt(4);
+					if (!(cmdchar >= 'a' && cmdchar <= 'z')) {
+						System.out.println("Input Error!!\nNo such operation!!");
+						System.exit(0);
+					}
 					ex.deri(cmdchar);
 					ex.merge();
+				}
+				else {
+					System.out.println("Input Error!!\nNo such operation!!");
 				}
 				ex.show();
 			}
 			// get expression
 			else {
-				/*
-				for (int i = 0; i < cmd.length(); i++) {
-					if (cmd.charAt(i) == '/') {
-						System.out.println("Error!");
-						System.exit(0);
-					}
-				}
-				*/
 				if (!ex.init(cmd)) {
 					System.out.println("Input Error!!");
 					System.exit(0);
